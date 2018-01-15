1f5267820338c7699962ec0260601308dfbc1123
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-01-15 09:50:09 -0300

    se agregan geter para retornar el contador por 2

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,14 +1,18 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <p>Counter is: {{ doubleCounter }}</p>
 </template>
 
 
 <script>
+  import { mapGetters } from 'vuex'
   export default {
   	computed: {
-  		counter () {
-  			return this.$store.state.counter
-  		}
-  	}
+      ...mapGetters([
+        'doubleCounter'
+      ]),
+      mycomputed () {
+        return 'holi'
+      }
+    }
   }
 </script>
